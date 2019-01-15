0ea9ca95edf86515ce13db72a17046fa4536e225
yangqintao <562439823@qq.com>
2019-01-15 23:39:52 +0800

    wordcount 程序添加local模式

--- helloworld/src/main/scala/com/yumtao/spark/WordCount.scala
+++ helloworld/src/main/scala/com/yumtao/spark/WordCount.scala
@@ -8,7 +8,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount {
   def main(args: Array[String]): Unit = {
     // 创建SparkConf()并设置App名称
-    val conf = new SparkConf().setAppName("WC")
+    val conf = new SparkConf().setAppName("WC").setMaster("local")
     // 创建SparkContext，该对象是提交spark App的入口
     val sc = new SparkContext(conf)
     sc.textFile(args(0)).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).sortBy(_._2, false).saveAsTextFile(args(1))
