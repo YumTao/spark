d97d41a164655015c5ab1e42dee1c2be482a56a1
YangQt <yangqt@zendaimoney.com>
2019-02-01 11:24:49 +0800

    [spark] 练习程序-两系统交易对账：类名修改，不止用于提现对账，也适用于充值对账

--- practice/src/main/scala/com/yumtao/ipfrom/TradeBalance.scala
+++ practice/src/main/scala/com/yumtao/ipfrom/TradeBalance.scala
@@ -8,7 +8,7 @@ import org.apache.spark.{SparkConf, SparkContext}
   * 比对两系统交易流水号，找出不同部分。
   * Created by yumtao on 2019/1/29.
   */
-object DrawBalance {
+object TradeBalance {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("DrawBalance").setMaster("local")
@@ -22,10 +22,10 @@ object DrawBalance {
     // wbs 提现成功rdd1(流水号, 金额)
     val wbsNo2Amt = sc.textFile(FileUtils.getPathInProject("draw/wbs_draw_success.txt"))
       .map(_.split("\t"))
-      .map(tmp => DrawObj(tmp(6), BigDecimal(tmp(1)).*(BigDecimal(100)).toInt))
+      .map(tmp => TradeObj(tmp(6), BigDecimal(tmp(1)).*(BigDecimal(100)).toInt))
 
     // ftp提现成功rdd2(流水号, 金额)
-    val ftpNo2Amt: RDD[DrawObj] = getDrawRddFromFtp(sc)
+    val ftpNo2Amt: RDD[TradeObj] = getDrawRddFromFtp(sc)
 
     // wbs - ftp，获取我方系统成功，第三方系统不成功的数据
     val wbsCutFtp = wbsNo2Amt.subtract(ftpNo2Amt)
@@ -59,9 +59,9 @@ object DrawBalance {
         }
       })
       .filter(option => option.isInstanceOf[Some[(String, String, String)]]).map(_.get)
-      .filter(_._3.equals("0000")).map(tmp => DrawObj(tmp._1, tmp._2.toInt))
+      .filter(_._3.equals("0000")).map(tmp => TradeObj(tmp._1, tmp._2.toInt))
   }
 }
 
 
-case class DrawObj(val thirdNo: String, val amt: Int) extends Serializable
+case class TradeObj(val thirdNo: String, val amt: Int) extends Serializable
